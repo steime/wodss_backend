e7cc753a6e44c5bbcd82b527dc367bdf9dcc086a
Dominik Steinmann <dominik.steinmann@students.fhnw.ch>
2020-02-21 16:58:34 +0100

    add gorilla mux router

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type User struct {
@@ -20,9 +23,12 @@ type DefaultRepository struct {
 }
 
 func main() {
-	http.HandleFunc("/user", userHandler)
-	http.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":8080", nil)
+	r := DefaultRepository{}
+	router := mux.NewRouter()
+	router.HandleFunc("/user", userHandler(&r)).Methods("GET")
+	router.HandleFunc("/", indexHandler).Methods("GET")
+	fmt.Printf("Server listening at :8080")
+	http.ListenAndServe(":8080", router)
 }
 
 func (r *DefaultRepository) AddUser(u User) {
@@ -33,11 +39,20 @@ func (r *DefaultRepository) AllUsers() []User {
 	return r.Users
 }
 
+/*
 func userHandler(w http.ResponseWriter, r *http.Request) {
 	u := User{Id: 1, Name: "Dominik"}
 	users := []User{u}
 	json, _ := json.Marshal(users)
 	w.Write(json)
+}*/
+
+func userHandler(repository Repository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		users := repository.AllUsers()
+		json, _ := json.Marshal(users)
+		w.Write(json)
+	}
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
